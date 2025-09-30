a4e2aeea157e3b3000fe8cdb950dc2d077f00c27
5ms <oms010414@gmail.com>
2025-09-30 23:43:23 +0900

    first commit

---
+++ src/main/java/glue/Gachi_Sanchaek/GachiSanchaekApplication.java
@@ -0,0 +1,13 @@
+package glue.Gachi_Sanchaek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachiSanchaekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachiSanchaekApplication.class, args);
+	}
+
+}
