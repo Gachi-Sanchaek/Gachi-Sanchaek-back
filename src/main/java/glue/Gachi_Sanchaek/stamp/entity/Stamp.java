7d38ef72991f77696c344b6c05639b7405df0f96
5ms <oms010414@gmail.com>
2025-10-07 16:16:22 +0900

    Feat: Implement Stamp entity and StampRepository

--- src/main/java/glue/Gachi_Sanchaek/stamp/entity/Stamp.java
+++ src/main/java/glue/Gachi_Sanchaek/stamp/entity/Stamp.java
@@ -1,15 +1,10 @@
-package glue.Gachi_Sanchaek.userStamp.entity;
+package glue.Gachi_Sanchaek.stamp.entity;
 
-import glue.Gachi_Sanchaek.stamp.entity.Stamp;
-import glue.Gachi_Sanchaek.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -21,28 +16,28 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
-@Table(name = "user_stamps")
+@Table(name = "stamps")
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class UserStamp {
+public class Stamp {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    @Column(nullable = false)
+    private String name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "stamp_id", nullable = false)
-    private Stamp stamp;
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private Long price = 500L;
 
     @CreationTimestamp
     @Column(name = "created_at")
     private LocalDateTime createdAt;
-
 }
---
+++ src/main/java/glue/Gachi_Sanchaek/stamp/repository/StampRepository.java
@@ -0,0 +1,7 @@
+package glue.Gachi_Sanchaek.stamp.repository;
+
+import glue.Gachi_Sanchaek.stamp.entity.Stamp;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StampRepository extends CrudRepository<Stamp, Long> {
+}
