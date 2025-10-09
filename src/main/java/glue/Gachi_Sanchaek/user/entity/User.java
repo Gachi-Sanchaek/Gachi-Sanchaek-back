fe772b7a7839f0fb2dafdeb0af48eba770903714
5ms <oms010414@gmail.com>
2025-10-09 16:43:27 +0900

    Refactor: remove UserStamp

--- src/main/java/glue/Gachi_Sanchaek/user/entity/User.java
+++ src/main/java/glue/Gachi_Sanchaek/user/entity/User.java
@@ -1,7 +1,6 @@
 package glue.Gachi_Sanchaek.user.entity;
 
 
-import glue.Gachi_Sanchaek.userStamp.entity.UserStamp;
 import glue.Gachi_Sanchaek.login.dto.UserJoinDto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -54,9 +53,6 @@ public class User {
     @CreationTimestamp
     @Column(name = "created_at")
     private LocalDateTime createdAt;
-
-    @OneToMany(mappedBy = "user")
-    private List<UserStamp> userStamps;
 
     @Column(name = "total_points", nullable = false)
     private Long totalPoints = 0L;
--- src/main/java/glue/Gachi_Sanchaek/userStamp/repository/UserStampRepository.java
+++
@@ -1,9 +0,0 @@
-package glue.Gachi_Sanchaek.userStamp.repository;
-
-import glue.Gachi_Sanchaek.userStamp.entity.UserStamp;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserStampRepository extends JpaRepository<UserStamp, Long> {
-    List<UserStamp> findByUserId(Long userId);
-}
