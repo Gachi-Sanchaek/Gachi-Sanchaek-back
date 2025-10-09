00f91e406f1bb6f450b62945a551d97d3367dea9
5ms <oms010414@gmail.com>
2025-10-09 19:48:53 +0900

    Feat: Implement pointLog entity and repository

---
+++ src/main/java/glue/Gachi_Sanchaek/pointLog/repository/PointLogRepository.java
@@ -0,0 +1,7 @@
+package glue.Gachi_Sanchaek.pointLog.repository;
+
+import glue.Gachi_Sanchaek.pointLog.entity.PointLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointLogRepository extends JpaRepository<PointLog, Long> {
+}
