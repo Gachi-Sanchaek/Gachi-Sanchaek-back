59dd78d1f1b9168efc29db105c61598834b1e706
zeehyee <amber9324@naver.com>
2025-10-28 16:52:54 +0900

    Feat: 사용자가 선택한 추천 산책로 저장 로직 구현

--- src/main/java/glue/Gachi_Sanchaek/walkRecommendation/controller/WalkRecommendationController.java
+++ src/main/java/glue/Gachi_Sanchaek/walkRecommendation/controller/WalkRecommendationController.java
@@ -1,16 +1,18 @@
 package glue.Gachi_Sanchaek.walkRecommendation.controller;
 
 import glue.Gachi_Sanchaek.common.ApiResponse;
+import glue.Gachi_Sanchaek.security.jwt.CustomUserDetails;
 import glue.Gachi_Sanchaek.user.entity.User;
+import glue.Gachi_Sanchaek.walkRecommendation.dto.SaveWalkRouteResponse;
 import glue.Gachi_Sanchaek.walkRecommendation.dto.WalkRecommendationGroupResponse;
+import glue.Gachi_Sanchaek.walkRecommendation.dto.WalkRouteSelectionRequest;
 import glue.Gachi_Sanchaek.walkRecommendation.service.WalkRecommendationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static glue.Gachi_Sanchaek.util.ApiResponse.ok;
 
 
 @RestController
@@ -29,6 +31,19 @@ public class WalkRecommendationController {
         return ResponseEntity.ok(ApiResponse.onSuccess(walkRecommendationService.recommendRoutes(
                 orgId, minutes,currentLat,currentLng)));
     }
+
+    @PostMapping("/select")
+    public ResponseEntity<ApiResponse<SaveWalkRouteResponse>> saveRoute(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestBody WalkRouteSelectionRequest req){
+        if (userDetails == null || userDetails.getUserId() == null) {
+            throw new org.springframework.security.access.AccessDeniedException("인증 정보가 유효하지 않거나 만료되었습니다.");
+        }
+        Long userId = userDetails.getUserId();
+        SaveWalkRouteResponse saved = walkRecommendationService.saveSelectedRoute(userId,req);
+        return ResponseEntity.ok(ApiResponse.onSuccess(saved));
+    }
+
 
 
 }
