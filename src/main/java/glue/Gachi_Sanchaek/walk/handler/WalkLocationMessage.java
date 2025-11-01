e3cfb8d86da050ebeca5c8fe4778981f2e483be7
siyun0520 <syun5520@naver.com>
2025-11-01 19:26:17 +0900

    Feat: 실시간 위치 업데이트 웹소켓 구현

---
+++ src/main/java/glue/Gachi_Sanchaek/walk/handler/WalkLocationMessage.java
@@ -0,0 +1,11 @@
+package glue.Gachi_Sanchaek.walk.handler;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class WalkLocationMessage {
+    private Long walkId;
+    private Double lat;
+    private Double lng;
+}
