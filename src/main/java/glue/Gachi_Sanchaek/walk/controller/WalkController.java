dce9e728fe91ca74176b08c0ad0338c1a54823dd
siyun <syun5520@naver.com>
2025-11-04 01:01:32 +0900

    Feat/AI 플로깅 인증 기능 구현 (#51)
    * Feat: 산책 종료 API 구현
    
    * Feat: VerificationMethod 추가
    
    * Feat: AI플로깅 인증 기능 구현
    
    * Feat: 이미지 사이즈 설정

--- src/main/java/glue/Gachi_Sanchaek/walk/controller/WalkController.java
+++ src/main/java/glue/Gachi_Sanchaek/walk/controller/WalkController.java
@@ -6,11 +6,13 @@ import glue.Gachi_Sanchaek.walk.dto.WalkEndRequest;
 import glue.Gachi_Sanchaek.walk.dto.WalkEndResponse;
 import glue.Gachi_Sanchaek.walk.dto.WalkResponse;
 import glue.Gachi_Sanchaek.walk.dto.WalkStartRequest;
+import glue.Gachi_Sanchaek.walk.service.GeminiWalkService;
 import glue.Gachi_Sanchaek.walk.service.WalkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("api/v1/walk")
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.*;
 
 public class WalkController {
     private final WalkService walkService;
+    private final GeminiWalkService geminiWalkService;
+
     @PostMapping("/start")
     public ResponseEntity<ApiResponse<WalkResponse>> startWalk(
             @RequestBody WalkStartRequest request,
@@ -33,6 +37,25 @@ public class WalkController {
         Long userId = Long.parseLong(userDetails.getUsername());
         WalkEndResponse response = walkService.endWalk(userId, request.getWalkId());
         return ApiResponse.ok(response,"산책 종료 성공");
+    }
+
+    @PostMapping("/plogging")
+    public ResponseEntity<ApiResponse<?>> pLogging(
+            @RequestParam("image") MultipartFile image,
+            @RequestParam("walkId") Long walkId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ){
+        Long userId = Long.parseLong(userDetails.getUsername());
+        int trashCount = geminiWalkService.countTrashImage(image);
+
+        if(trashCount >= 10){
+            WalkEndResponse response = walkService.endWalk(userId, walkId);
+            return (ResponseEntity<ApiResponse<?>>) (ResponseEntity<?>) ApiResponse.ok(response, "플로깅 검증 성공");
+        }
+        else{
+            String message = "플로깅 검증 실패. 10개 이상의 쓰레기가 필요합니다";
+            return (ResponseEntity<ApiResponse<?>>) (ResponseEntity<?>) ApiResponse.badRequest(message);
+        }
     }
 
 }
