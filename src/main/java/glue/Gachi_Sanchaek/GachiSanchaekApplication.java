422c7c9feab51fd000672a1145676e5da5c2ba1e
5ms <86279729+oms01@users.noreply.github.com>
2025-11-13 13:50:15 +0900

    Revert "Feat/97 cd (#98)"
    This reverts commit f7f0af384798c6960f3f090708b5cb301fe9cf59.

--- src/main/java/glue/Gachi_Sanchaek/GachiSanchaekApplication.java
+++ src/main/java/glue/Gachi_Sanchaek/GachiSanchaekApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class GachiSanchaekApplication {
 
 	public static void main(String[] args) {
+
         SpringApplication.run(GachiSanchaekApplication.class, args);
 	}
 
