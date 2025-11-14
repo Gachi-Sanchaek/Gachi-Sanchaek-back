d2862fb68e1bc81209e30ee7a2a360afa5e7101e
5ms <oms010414@gmail.com>
2025-11-14 14:05:32 +0900

    fix OpenApiConfig

--- src/main/java/glue/Gachi_Sanchaek/docs/config/OpenApiConfig.java
+++ src/main/java/glue/Gachi_Sanchaek/docs/config/OpenApiConfig.java
@@ -29,7 +29,7 @@ public class OpenApiConfig {
                         .version("v1.0.0"))
                 .servers(List.of(
                         new Server()
-                                .url("http://13.124.32.133:8080")
+                                .url("https://13.124.32.133:8080")
                                 .description("운영 서버"),
                         new Server()
                                 .url("http://localhost:8080")
